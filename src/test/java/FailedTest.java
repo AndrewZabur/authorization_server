65cd50b288ffef5b91d8cfeb859710dc28794766
Andrew Zaburyanniy <andrewzabur@gmail.com>
2022-11-24 23:01:35 +0200

    failed test

---
+++ src/test/java/FailedTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FailedTest {
+
+    @Test
+    public void shouldFail() {
+        assertEquals(2, 3);
+    }
+
+}
